62c2c0b788f57294402bbfed7efc29d5b2dcbf66
smrk <ronm99@yahoo.com>
2022-08-01 17:40:50 -0700

    Add support for smrk100g.

---
+++ MyWork/shell_module/src/led.c
@@ -0,0 +1,132 @@
+#include <stdlib.h>
+#include <zephyr/shell/shell.h>
+#include <sys/byteorder.h>
+
+#include "lib.h"
+#define LED_ADDR 0x43
+
+/*
+ * Control individual LEDs (0-7)
+ *
+ * led LED <on|green|yellow|off>
+ * LED: D8, D9, D10, D11
+ * on is both LEDS on
+ * D8 is closest to board, D11 furthest
+ */
+struct lmap {
+	char *name;
+	int  ypin; /* yellow */
+	int  gpin; /* green */
+	unsigned char mask; /* mask bits */
+} ledmap[] = {
+	{"d8", 3, 2, 0xfc},
+	{"d9", 1, 0, 0xf3},
+	/* {"d10", 1, 0, 0xfc}, */
+	/* {"d11", 6, 7, 0x3f}, */
+	{0, 0, 0, 0}
+};
+	
+		
+static int cmd_led(const struct shell *sh, size_t argc, char **argv)
+{
+	char s[10];
+	char buf[20];
+
+	struct lmap *p;
+	char *name = 0;
+	int ypin = 0, gpin = 0;
+	char gval = 0, tmp;
+	unsigned char mask = 0;
+	int all = 0;
+	char gall = 0, yall = 0, amask = 0;
+	
+	p = &ledmap[0];
+	strcpy(s, toLower(argv[1]));
+	for (; p->name != NULL; p++) {
+		if (strcmp(p->name, s) == 0 ) {
+			name = p->name;
+			ypin = p->ypin;
+			gpin = p->gpin;
+			mask = p->mask;
+		}
+		gall |= (1 << p->gpin);
+		yall |= (1 << p->ypin);
+		amask |= p->mask;
+	}
+	if (!name) {
+		if (s[0] == 'a') {
+			all = 1;
+			name = "all";
+		} else {
+			printk("Not found: %s\n", s);
+			return -ENOENT;
+		}
+	}
+	printk("name: %s, y=%d, g=%d\n", name, ypin, gpin);
+	printk("yall=0x%x, gall = 0x%x, mask = 0x%02x\n", gall, yall, amask);
+	strcpy(s, toLower(argv[2]));
+	switch (s[0]) {
+	case 'g':
+		if (all)
+			gval = gall;
+		else
+			gval = 1 << gpin;
+		break;
+	case 'y':
+		if (all)
+			gval = yall;
+		else
+			gval = 1 << ypin;
+		break;
+	case 'o':
+		if (s[1] == 'n') {	/* on */
+			if (all)
+				gval |= (gall|yall);
+			else
+				gval = (1 << gpin) | (1 << ypin);
+			break;
+		} else if (s[1] == 'f') {
+			gval = 0;
+			break;
+		} else {
+			printk("Bad option: %s\n", s);
+			return -ENOENT;
+		}
+	default:
+		printk("Bad option: %s\n", s);
+		return -ENOENT;
+
+	}
+
+	if (all)
+		mask = amask;
+
+	printk("gval = 0x%02x\n", gval);
+	printk("mask = 0x%02x\n", mask);
+	/* LED Controller is on I2C Bus 5 */
+	set_mux(5);
+
+	/* Set all pins to output */	
+	buf[0] = 0xff;
+	i2c_write_bytes(LED_ADDR, 3, buf, 1);
+	/* High impedence off */
+	buf[0] = 0;
+	i2c_write_bytes(LED_ADDR, 7, buf, 1);
+	/* disbale PU/PD */
+	buf[0] = 0;
+	i2c_write_bytes(LED_ADDR, 0xb, buf, 1);
+	/* write data */
+
+	i2c_read_bytes(LED_ADDR, 5, buf, 1);
+	printk("read 0x%02x\n", buf[0]);
+
+	tmp = buf[0];
+	buf[0] = (tmp & !mask) | gval;
+	printk("writing 0x%02x\n", buf[0]);
+	
+	i2c_write_bytes(LED_ADDR, 5, buf, 1);
+
+	return 0;
+}
+
+SHELL_CMD_ARG_REGISTER(led, NULL, "Set LEDS", cmd_led, 3, 0);
---
+++ MyWork/shell_module/src/main.c
@@ -0,0 +1,177 @@
+/*
+ * Copyright (c) 2015 Intel Corporation
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <zephyr/zephyr.h>
+#include <zephyr/shell/shell.h>
+#include <version.h>
+#include <zephyr/logging/log.h>
+#include <stdlib.h>
+#include <zephyr/drivers/uart.h>
+#include <zephyr/usb/usb_device.h>
+#include <ctype.h>
+#include "lib.h"
+
+LOG_MODULE_REGISTER(app);
+
+#if defined CONFIG_SHELL_GETOPT
+/* Thread save usage */
+static int cmd_demo_getopt_ts(const struct shell *sh, size_t argc,
+			      char **argv)
+{
+	struct getopt_state *state;
+	char *cvalue = NULL;
+	int aflag = 0;
+	int bflag = 0;
+	int c;
+
+	while ((c = getopt(argc, argv, "abhc:")) != -1) {
+		state = getopt_state_get();
+		switch (c) {
+		case 'a':
+			aflag = 1;
+			break;
+		case 'b':
+			bflag = 1;
+			break;
+		case 'c':
+			cvalue = state->optarg;
+			break;
+		case 'h':
+			/* When getopt is active shell is not parsing
+			 * command handler to print help message. It must
+			 * be done explicitly.
+			 */
+			shell_help(sh);
+			return SHELL_CMD_HELP_PRINTED;
+		case '?':
+			if (state->optopt == 'c') {
+				shell_print(sh,
+					"Option -%c requires an argument.",
+					state->optopt);
+			} else if (isprint(state->optopt)) {
+				shell_print(sh,
+					"Unknown option `-%c'.",
+					state->optopt);
+			} else {
+				shell_print(sh,
+					"Unknown option character `\\x%x'.",
+					state->optopt);
+			}
+			return 1;
+		default:
+			break;
+		}
+	}
+
+	shell_print(sh, "aflag = %d, bflag = %d", aflag, bflag);
+	return 0;
+}
+
+static int cmd_demo_getopt(const struct shell *sh, size_t argc,
+			      char **argv)
+{
+	char *cvalue = NULL;
+	int aflag = 0;
+	int bflag = 0;
+	int c;
+
+	while ((c = getopt(argc, argv, "abhc:")) != -1) {
+		switch (c) {
+		case 'a':
+			aflag = 1;
+			break;
+		case 'b':
+			bflag = 1;
+			break;
+		case 'c':
+			cvalue = optarg;
+			break;
+		case 'h':
+			/* When getopt is active shell is not parsing
+			 * command handler to print help message. It must
+			 * be done explicitly.
+			 */
+			shell_help(sh);
+			return SHELL_CMD_HELP_PRINTED;
+		case '?':
+			if (optopt == 'c') {
+				shell_print(sh,
+					"Option -%c requires an argument.",
+					optopt);
+			} else if (isprint(optopt)) {
+				shell_print(sh, "Unknown option `-%c'.",
+					optopt);
+			} else {
+				shell_print(sh,
+					"Unknown option character `\\x%x'.",
+					optopt);
+			}
+			return 1;
+		default:
+			break;
+		}
+	}
+
+	shell_print(sh, "aflag = %d, bflag = %d", aflag, bflag);
+	return 0;
+}
+
+static int cmd_demo_params(const struct shell *shell, size_t argc, char **argv)
+{
+	shell_print(shell, "argc = %zd", argc);
+	for (size_t cnt = 0; cnt < argc; cnt++) {
+		shell_print(shell, "  argv[%zd] = %s", cnt, argv[cnt]);
+	}
+
+	return 0;
+}
+
+static int cmd_demo_hexdump(const struct shell *shell, size_t argc, char **argv)
+{
+	shell_print(shell, "argc = %zd", argc);
+	for (size_t cnt = 0; cnt < argc; cnt++) {
+		shell_print(shell, "argv[%zd]", cnt);
+		shell_hexdump(shell, argv[cnt], strlen(argv[cnt]));
+	}
+
+	return 0;
+}
+#endif
+
+static int cmd_version(const struct shell *shell, size_t argc, char **argv)
+{
+	ARG_UNUSED(argc);
+	ARG_UNUSED(argv);
+
+	shell_print(shell, "Zephyr version %s", KERNEL_VERSION_STRING);
+
+	return 0;
+}
+
+SHELL_CMD_ARG_REGISTER(version, NULL, "Show kernel version", cmd_version, 1, 0);
+
+void main(void)
+{
+	printk("\nWelcome to smrk100g");
+
+	setup_dev(DEV_SET);
+	enable_vdd_3r3();
+
+#if DT_NODE_HAS_COMPAT(DT_CHOSEN(zephyr_shell_uart), zephyr_cdc_acm_uart)
+	const struct device *dev;
+	uint32_t dtr = 0;
+
+	dev = DEVICE_DT_GET(DT_CHOSEN(zephyr_shell_uart));
+	if (!device_is_ready(dev) || usb_enable(NULL)) {
+		return;
+	}
+
+	while (!dtr) {
+		uart_line_ctrl_get(dev, UART_LINE_CTRL_DTR, &dtr);
+		k_sleep(K_MSEC(100));
+	}
+#endif
+}
